2fbaf79736f54c10daf4d6e62b938c546dc6ef17
José Lucas <58404702+joselucasoficial@users.noreply.github.com>
2022-01-19 17:20:58 -0300

    more

---
+++ tex.js
@@ -0,0 +1,10 @@
+function find(nums){
+    let max_num = number.NEGATIVE_INFINITY
+    // menor que todos os numeros
+    for(let num of nums){
+        if(num > max_num){
+            
+        }
+    }
+    return max_num
+}
